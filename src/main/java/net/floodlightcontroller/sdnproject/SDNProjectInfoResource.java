0403eee60910b151c63b9c64602f66ad72775841
m1k3lin0 <m.debonis991@gmail.com>
2016-05-06 17:29:25 +0200

    implementation of the info resource (servers table filled with random values inside the printclient functions)

--- src/main/java/net/floodlightcontroller/sdnproject/SDNProjectInfoResource.java
+++ src/main/java/net/floodlightcontroller/sdnproject/SDNProjectInfoResource.java
@@ -19,6 +19,8 @@ import org.slf4j.LoggerFactory;
  * responds to URI /info
  * returns the list of server belonging to the client
  */
+
+
 public class SDNProjectInfoResource extends ServerResource {
 	protected static Logger log = LoggerFactory.getLogger(SDNProjectRequestResource.class);
 	
@@ -26,7 +28,13 @@ public class SDNProjectInfoResource extends ServerResource {
 	public String printClients(String jsonData){
 		IStorageSourceService storageSource = 
 				(IStorageSourceService)getContext().getAttributes().get(IStorageSourceService.class.getCanonicalName());
-		
+
+		/* TODO
+		 * parse jsonData to get username
+		 * if username doesn't exist in table -> return error
+		 * query servers table to find all servers associated to user
+		 * return list of server's addresses in json format
+		 * */
 		Map<String, Object> data = new HashMap<String, Object>();
 		String ret = null;
 		
@@ -38,6 +46,7 @@ public class SDNProjectInfoResource extends ServerResource {
 		
 		log.info("received json: " + jsonData);
 		
+		/* parse json data */
 		try {
 			data = SDNProject.jParse(jsonData);
 		}
@@ -45,37 +54,47 @@ public class SDNProjectInfoResource extends ServerResource {
 			log.error("error while parsing received data: " + jsonData, e);
 			return "{\"status\" : \"Error retrieving client info, see log for details.\"}";
 		}
-		String client = (String)data.get(SDNProject.COLUMN_U_NAME); //
-		OperatorPredicate predicate = new OperatorPredicate(SDNProject.COLUMN_S_USER,OperatorPredicate.Operator.EQ,client);
 		
+		String user = (String)data.get(SDNProject.COLUMN_U_NAME); //
+		
+		/* INSERIMENTI DI PROVA ---> VANNO TOLTI PERCHE' LA TABELLA NON VIENE RIEMPITA QUI */
 		Map<String,Object> values = new HashMap<String,Object>();
 		values.put(SDNProject.COLUMN_S_ID, "ALEXNONECAPACE");
 		values.put(SDNProject.COLUMN_S_USER, "trignoleomerda");
 		values.put(SDNProject.COLUMN_S_VIRTUAL, "indirizzo di prova");
-		storageSource.insertRowAsync(SDNProject.TABLE_SERVERS, values); //crea una nuova riga nella tabella users
+		Map<String,Object> values2 = new HashMap<String,Object>();
+		values2.put(SDNProject.COLUMN_S_ID, "ALEXNONECAPACE2");
+		values2.put(SDNProject.COLUMN_S_USER, "trignoleomerda");
+		values2.put(SDNProject.COLUMN_S_VIRTUAL, "indirizzo di prova2");
+		Map<String,Object> values3 = new HashMap<String,Object>();
+		values3.put(SDNProject.COLUMN_S_ID, "ALEXNONECAPACE3");
+		values3.put(SDNProject.COLUMN_S_USER, "trignoleomerda");
+		values3.put(SDNProject.COLUMN_S_VIRTUAL, "indirizzo di prova3");
+		storageSource.insertRowAsync(SDNProject.TABLE_SERVERS, values); //crea una nuova riga nella tabella servers
+		storageSource.insertRowAsync(SDNProject.TABLE_SERVERS, values2); //crea una nuova riga nella tabella servers
+		storageSource.insertRowAsync(SDNProject.TABLE_SERVERS, values3); //crea una nuova riga nella tabella servers
+		/*-----------------------------------------------------------------*/
 		
+		/* check if username exists */
+		if(SDNUtils.userExists(storageSource, user)) {
+			log.error("error while fetching information, user {} not existent!", user);
+			return "{\"status\" : \"User not existent, check log for details \"}";
+		}
 		
-//		Map<String,Object> row;
-		/*ret = 	"{ \"user\" : \" " + data.get(SDNProject.COLUMN_U_NAME) + "\","
-				+ "\"servers\" : \" " + data.get(SDNProject.COLUMN_U_SERVERS) + "\""
-				+ "}";
-		/* TODO
-		 * parse jsonData to get username
-		 * if username doesn't exist in table -> return error
-		 * query servers table to find all servers associated to user
-		 * return list of server's addresses in json format
-		 * */
-		
+		/* create the answer in json format */
+		log.info("creating json answer");
+		ret = "{ \"user\" : \"" + user + "\", \"servers\" : [";
+		/* query the servers table */
+		OperatorPredicate predicate = new OperatorPredicate(SDNProject.COLUMN_S_USER,OperatorPredicate.Operator.EQ,user);
 		IResultSet resultSet = storageSource.executeQuery(SDNProject.TABLE_SERVERS, 
 			new String[] {SDNProject.COLUMN_S_VIRTUAL}, predicate, null);
 		Map<String, Object> row;
-		int i=0;
-		for (Iterator<IResultSet> it = resultSet.iterator(); it.hasNext(); i++) {
+		for (Iterator<IResultSet> it = resultSet.iterator(); it.hasNext(); ) {
 			row = it.next().getRow();
-			log.info("PORCO DUEEEEEEEEEEEEEEEEEEEEEEEEEEEE **** ROW(" + i + ") = " + row.get(SDNProject.COLUMN_S_VIRTUAL).toString());
+			ret += "\""+row.get(SDNProject.COLUMN_S_VIRTUAL).toString()+"\"";
+			if(it.hasNext()) ret+= ",";
 		}
-		
-		
+		ret += "] }";
 
 		return ret;
 	}
